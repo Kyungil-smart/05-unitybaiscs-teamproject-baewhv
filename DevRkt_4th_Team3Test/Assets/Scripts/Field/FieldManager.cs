d42c1c5c330bf49ba00ece76617644730e03196b
baewhv <baewhv@naver.com>
2026-01-29 02:22:45 +0900

    feat : 타일 맵 무한 루프 구현 완료

--- DevRkt_4th_Team3Test/Assets/Scripts/Field/FieldManager.cs
+++ DevRkt_4th_Team3Test/Assets/Scripts/Field/FieldManager.cs
@@ -7,22 +7,33 @@ public class FieldManager : MonoBehaviour
     [SerializeField] private GameObject _fieldTile;
     [SerializeField] private GameObject _designedTile;
     private List<List<FieldTile>> _tiles = new List<List<FieldTile>>();
-    private const int _mapWidth = 11;
-    private const int _mapHeight = 11;
-    private const int _mapSightWidth = 2;
-    private const int _mapSightHeight = 2;
+    //루프시킬 맵의 최대 크기
+    [SerializeField] private Vector2Int _preLoadMapSize = new Vector2Int(11,11);
+    //활성화시킬 맵의 크기
+    [SerializeField] private Vector2Int _mapSight = new Vector2Int(2,2);
 
     private readonly (int x, int z) tileSize = (16, 16);
     private static FieldManager _instance;
 
-    private Vector2 _playerTilePosition;
+    private Vector2Int _playerPosition;
+    private Vector2Int _playerTilePosition;
 
-    public Vector2 PlayerTilePosition
+    public Vector2Int PlayerTilePosition
     {
         get => _playerTilePosition;
         set
         {
             if (_playerTilePosition == value) return;
+            Vector2Int temp = value - _playerTilePosition;
+            if (temp.x == _preLoadMapSize.x - 1)
+                temp.x = -1;
+            else if (temp.x == -(_preLoadMapSize.x - 1))
+                temp.x = 1;
+            if (temp.y == _preLoadMapSize.y - 1)
+                temp.y = -1;
+            else if (temp.y == -(_preLoadMapSize.y - 1))
+                temp.y = 1;
+            _playerPosition += temp;
             LoadTile(value);
             _playerTilePosition = value;
         }
@@ -39,7 +50,7 @@ public class FieldManager : MonoBehaviour
 
     private void Start()
     {
-        PlayerTilePosition = Vector2.zero;
+        PlayerTilePosition = Vector2Int.zero;
         LoadTile(PlayerTilePosition);
     }
 
@@ -63,34 +74,49 @@ public class FieldManager : MonoBehaviour
     //맵 생성
     private void InitMap()
     {
-        for (int y = 0; y <= _mapHeight; y++)
+        for (int y = 0; y < _preLoadMapSize.y; y++)
         {
             _tiles.Add(new List<FieldTile>());
-            for (int x = -_mapWidth / 2; x <= _mapWidth / 2; x++)
+            for (int x = 0; x < _preLoadMapSize.x; x++)
             {
                 FieldTile tile = Instantiate(_fieldTile).GetComponent<FieldTile>();
                 tile.transform.SetParent(gameObject.transform);
-                tile.TilePosition = new Vector2(x, y - _mapHeight / 2);
+                tile.TilePosition = new Vector2Int(x, y);
                 _tiles[y].Add(tile);
             }
         }
     }
 
-    // 캐릭터가 움직였을 때 
-    private void LoadTile(Vector2 tilePos)
+    // 캐릭터가 움직인 위치를 기준으로 _mapSight만큼 활성화.
+    // _mapSight에 해당하는 타일에 디자인 타일이 등록되지 않았다면 생성 및 초기화.
+    private void LoadTile(Vector2Int tilePos) //0 0 
     {
-        for (int y = (int)tilePos.y - _mapSightHeight; y <= (int)tilePos.y + _mapSightHeight; y++)
+        Debug.Log($"player pos {_playerPosition} / tile pos {tilePos}");
+        for (int y = 0; y <= _mapSight.y * 2 + 2; y++)
         {
-            for (int x = (int)tilePos.x - _mapSightWidth; x <= (int)tilePos.x + _mapSightWidth; x++)
+            for (int x = 0; x <= _mapSight.x * 2 + 2; x++)
             {
-                int posX = (_mapWidth + x) % _mapWidth;
-                int posY = (_mapHeight + y) % _mapHeight;
-                if (!_tiles[posY][posX].isSetTile)
+                int _x = tilePos.x - _mapSight.x - 1 + x;
+                int _y = tilePos.y - _mapSight.y - 1 + y;
+                int playerPos_X = _playerPosition.x - _mapSight.x - 1 + x;
+                int playerPos_Y = _playerPosition.y - _mapSight.y - 1 + y;
+                int posX = (_x + _preLoadMapSize.x) % _preLoadMapSize.x;
+                int posY = (_y + _preLoadMapSize.y) % _preLoadMapSize.y;
+                if (x == 0 || y == 0 || x == _mapSight.x * 2 + 2 || y == _mapSight.y * 2 + 2) //테두리 제거
                 {
-                    _tiles[posY][posX].TileDesign = Instantiate(_designedTile);
+                    _tiles[posY][posX].DisableFieldTile();
+                }
+                else if (!_tiles[posY][posX].isSetTile) //없을 때 생성
+                {
+                    _tiles[posY][posX].TileDesign = Instantiate(_designedTile); //TODO 타일 종류 추가 시 랜덤 생성
                     _tiles[posY][posX].TileDesign.transform.SetParent(_tiles[posY][posX].transform);
-                    _tiles[posY][posX].transform.position = new Vector3(x * tileSize.x, 0, y * tileSize.z);
-                    _tiles[posY][posX].StartFieldTile();
+                    _tiles[posY][posX].transform.position = new Vector3(playerPos_X * tileSize.x, 0, playerPos_Y * tileSize.z);
+                    _tiles[posY][posX].EnableFieldTile();
+                }
+                else if (_tiles[posY][posX].TileDesign.activeSelf == false) //꺼져 있을 때 다시 켜기
+                {
+                    _tiles[posY][posX].transform.position = new Vector3(playerPos_X * tileSize.x, 0, playerPos_Y * tileSize.z);
+                    _tiles[posY][posX].EnableFieldTile();
                 }
             }
         }
--- DevRkt_4th_Team3Test/Assets/Scripts/Field/FieldTile.cs
+++ DevRkt_4th_Team3Test/Assets/Scripts/Field/FieldTile.cs
@@ -1,30 +1,39 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class FieldTile : MonoBehaviour
 {
-    public Vector2 TilePosition { get; set; }
+    [field:SerializeField] public Vector2Int TilePosition { get; set; }
     public GameObject TileDesign { get; set; }
 
     public bool isSetTile => TileDesign != null;
 
-    public void StartFieldTile()
+    /// <summary>
+    /// 필드 활성화
+    /// </summary>
+    public void EnableFieldTile()
     {
         GetComponent<BoxCollider>().enabled = true;
-        if(TileDesign) TileDesign.SetActive(true);
+        if (TileDesign) TileDesign.SetActive(true);
+    }
+    /// <summary>
+    /// 필드 비활성화
+    /// </summary>
+    public void DisableFieldTile()
+    {
+        GetComponent<BoxCollider>().enabled = false;
+        if (TileDesign) TileDesign.SetActive(false);
     }
 
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other);
         if (other.CompareTag("Player"))
         {
-            Debug.Log(TilePosition);
             FieldManager.Instance.PlayerTilePosition = TilePosition;
         }
     }
-
 }
