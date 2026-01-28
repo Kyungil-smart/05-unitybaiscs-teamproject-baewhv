a328622172cf3e3edb7f2f631b4873851b10c404
sjungah <sjungah99@gmail.com>
2026-01-28 10:43:14 +0900

    fix : level ui

---
+++ DevRkt_4th_Team3Test/Assets/Scripts/ui/LevelUI.cs
@@ -0,0 +1,35 @@
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine;
+
+public class LevelUI : MonoBehaviour
+{
+    [Header("UI")]
+    [SerializeField]private Image _gauge;
+    [SerializeField]private TextMeshProUGUI _expText;
+    [SerializeField]private TextMeshProUGUI _levelText;
+    public float _lerpSpeed = 5f;
+    
+    [Header("Data")]
+    [SerializeField]private ExpSystem _expSystem;
+    
+    void Update()
+    {
+        UpdateExpGauge();
+    }
+    
+    private void UpdateExpGauge()
+    {
+        float targetFill = (float)_expSystem.CurrentExp / _expSystem.ExpToNextLevel;
+        
+        _gauge.fillAmount = Mathf.Lerp(_gauge.fillAmount, targetFill, Time.deltaTime * _lerpSpeed);
+
+        if (_expText != null)
+            _expText.text = $"EXP {_expSystem.CurrentExp} / {_expSystem.ExpToNextLevel}";
+
+        if (_levelText != null)
+            _levelText.text = $"{_expSystem.Level}";
+    }
+    
+    
+}
