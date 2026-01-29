8123f9a9923bccb021f16989eb49031082a5b42c
xkiolp <51228101+xkiolp@users.noreply.github.com>
2026-01-29 16:24:33 +0900

    feat/weapon 근접무기 구현중.

---
+++ DevRkt_4th_Team3Test/Assets/Scripts/Weapon/RangedWeaponManager.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangedWeaponManager : MonoBehaviour
+{
+    public static RangedWeaponManager RangedInstance;
+
+    // 인스펙터에서 배치한 RangedWeapon 스크립트들을 할당
+    public List<GameObject> _rangedWeaponPrefabs = new List<GameObject>();
+    // 실제 게임 내에서 관리될 생성된 무기들
+    public List<RangedWeapon> _rangedWeapons = new List<RangedWeapon>();
+    public GameObject _player;
+
+    private void Awake()
+    {
+        if (RangedInstance != null && RangedInstance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            RangedInstance = this; // ....
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        CreatePrefabsObject();
+    }
+    
+    // 프리팹으로부터 객체를 만들어서 _rangedWeapons에 등록하는 함수
+    private void CreatePrefabsObject()
+    {
+        for (int i = 0; i < _rangedWeaponPrefabs.Count; i++)
+        {
+            
+            if (_rangedWeaponPrefabs[i] != null)
+            {
+                GameObject go = Instantiate(_rangedWeaponPrefabs[i], transform);
+                RangedWeapon rw = go.GetComponent<RangedWeapon>();
+                _rangedWeapons.Add(rw);
+            }
+        }
+    }
+}
--- DevRkt_4th_Team3Test/Assets/Scripts/Weapon/WeaponManager.cs
+++ DevRkt_4th_Team3Test/Assets/Scripts/Weapon/WeaponManager.cs
@@ -22,7 +22,13 @@ public class WeaponManager : MonoBehaviour
             return;
         }
     }
-    
+
+    private void Start()
+    {
+        //리스트를 0.1초 있다가 읽어옵니다.
+        StartCoroutine(FirstGetWeaponList(0.1f));
+        
+    }
     /// <summary>
     /// 싱글톤으로 선언된 3종류의 WeaponManager로부터 무기들 종류를 받아서 List<WeaponBase> weapons에다가 등록한다.
     /// </summary>
@@ -36,5 +42,20 @@ public class WeaponManager : MonoBehaviour
             weapons.Add(OrbitalWeaponManager.OrbitalInstance._orbitalWeapons[i]);
         }
         //다른무기들 만들면 다른무기들도 나중에 가져온다.
+        for (int i = 0; i < RangedWeaponManager.RangedInstance._rangedWeapons.Count; i++)
+        {
+            weapons.Add(RangedWeaponManager.RangedInstance._rangedWeapons[i]);
+        }
+
+        // for (int i = 0; i < weapons.Count; i++)
+        // {
+        //     Debug.Log(weapons[i]._weaponName);
+        // }
+    }
+
+    private IEnumerator FirstGetWeaponList(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        GetWeaponList();
     }
 }
