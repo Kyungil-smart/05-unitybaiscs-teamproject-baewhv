730764e0ef1990938c3263d41d7342b8c731f124
baewhv <baewhv@naver.com>
2026-01-29 14:17:59 +0900

    feat : 필드 오브젝트 추가. 장애물 및 습득형 아이템 초안 추가.

---
+++ DevRkt_4th_Team3Test/Assets/Scripts/Tools/Singleton.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton <T>: MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType<T>();
+                if (_instance == null)
+                {
+                    _instance = new GameObject().AddComponent<T>();
+                }
+            }
+            DontDestroyOnLoad(_instance.gameObject);
+            return _instance;
+        }
+    }
+
+    protected void SingletonInit()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            _instance = this as T; //업캐스팅
+        }
+    }
+}
