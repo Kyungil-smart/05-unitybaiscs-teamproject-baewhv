929df23564fe8bc553b602a721441ce06353058b
xkiolp <51228101+xkiolp@users.noreply.github.com>
2026-01-29 10:28:11 +0900

    MonsterState랑 연결될수 있게 수정

--- DevRkt_4th_Team3Test/Assets/Scripts/Weapon/MeleeWeapon.cs
+++ DevRkt_4th_Team3Test/Assets/Scripts/Weapon/MeleeWeapon.cs
@@ -1,33 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class OrbitalWeapon : WeaponBase, IWeapon
+public class MeleeWeapon : WeaponBase, IWeapon
 {
-    private void Start()
-    {
-        
-    }
-    
-    private void Update()
-    {
-        //캐릭터의 위치를 기준으로 공전
-    }
+    //MeleeWeapon은 WeaponBase 전투스탯의 weaponDamage, weaponAttackSpeed만 씁니다
     private void OnTriggerEnter(Collider collider)
     {
         //충돌한 물체가 enemy이면 공격
-        if (!collider.CompareTag("Enemy"))
+        if (!collider.CompareTag("WeaponEnemy"))
         {
             return;
         } 
             
         
-        MonsterState monster = collider.GetComponent<MonsterState>();
+        WeaponMonster monster = collider.GetComponent<WeaponMonster>();
         // 컴포넌트가 없으면 리턴
         if (monster == null) return;
-        monster.TakeDamage(weaponDamage);
-
+        
+        // 공격 실행
+        WeaponAttack(monster);
     }
     
     
@@ -41,5 +33,4 @@ public class OrbitalWeapon : WeaponBase, IWeapon
         monster.HP -= weaponDamage;
         Debug.Log($"HP : {monster.HP}");
     }
-    
 }
---
+++ DevRkt_4th_Team3Test/Assets/Scripts/Weapon/MeleeWeaponManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeaponManager : MonoBehaviour
+{
+    public static MeleeWeaponManager MeleeInstance;
+    //무기 프리펩 저장
+    [SerializeField] private List<GameObject> prefabs =  new List<GameObject>();
+    //궤도무기들 종류마다 저장
+    public List<MeleeWeapon> _meleeWeapons = new List<MeleeWeapon>();
+    [SerializeField] private GameObject _player;
+
+    private float timer = 0;
+    //플레이어 따라다님. transform.position = _player.transform.position
+    //기본값f/공격속도의 속도로 데미지만큼 공격, (OntriggerEnter에서 특정초가 지날때마다 공격? 코루틴?
+    //start부터 시작하는 타이머 - (공격속도 바뀌는것은 타이머 사이클 끝난직후부터 적용.)
+    
+    private void Start()
+    {
+        
+    }
+    
+    private void Update()
+    {
+        timer += Time.deltaTime;
+    }
+    
+    
+}
