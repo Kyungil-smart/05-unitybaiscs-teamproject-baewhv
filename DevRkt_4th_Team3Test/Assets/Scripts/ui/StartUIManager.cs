71959c5ea750b9cd01df347fac4a67c1ba62eadf
sjungah <sjungah99@gmail.com>
2026-01-29 10:47:29 +0900

    add : 시작화면 소개 버튼, 팝업 구현

--- DevRkt_4th_Team3Test/Assets/Scripts/ui/StartUIManager.cs
+++ DevRkt_4th_Team3Test/Assets/Scripts/ui/StartUIManager.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class StartUIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _infoPopup;
     private DataController _dataController;
-    [SerializeField] private GameObject _settingPopup;
+    private GameObject _currentInfoPopup;
+    private InfoPopupUI  _infoPopupUI;
+    public void Awake()
+    {
+        //팝업 프리팹 생성해서 할당
+        if (_currentInfoPopup == null)
+        {
+            // Instantiate: 게임 오브젝트를 동적으로 생성
+            _currentInfoPopup = Instantiate(_infoPopup, GameObject.Find("Canvas").transform);
+            // 생성 직후 화면에서 숨김
+            _currentInfoPopup.SetActive(false);
+        }
 
+        _infoPopupUI = _currentInfoPopup.GetComponent<InfoPopupUI>();
+    }
+    
     public void StartGame()
     {
         SceneManager.LoadScene(1);
@@ -21,7 +36,7 @@ public class StartUIManager : MonoBehaviour
     
     public void OpenOptions()
     {
-        
+        _infoPopupUI.Open();
     }
     
     /// <summary>
