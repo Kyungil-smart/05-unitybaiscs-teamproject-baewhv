161d7a3e15868f672ed042f2e7e3a68b091d924c
sjungah <sjungah99@gmail.com>
2026-02-02 11:41:15 +0900

    add : 무기 인벤토리 HUD 추가

--- DevRkt_4th_Team3Test/Assets/Scripts/ui/InventorySlot.cs
+++ DevRkt_4th_Team3Test/Assets/Scripts/ui/InventorySlot.cs
@@ -2,24 +2,12 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class InventorySlot
+public class InventorySlot : MonoBehaviour
 {
     public Image itemIcon;
-    public TextMeshProUGUI itemNameText;
-    public TextMeshProUGUI itemCountText;
     
-    public void SetItem(string name, int count)
+    public void SetItem(WeaponBase weapon)
     {
-        itemNameText.text = name;
-        itemCountText.text = count.ToString();
-        //TODO: 이미지 추가 작업
-        // itemIcon.sprite = Resources.Load<Sprite>($"Icons/{name}");
-    }
-    
-    public void ClearSlot()
-    {
-        itemNameText.text = "";
-        itemCountText.text = "";
-        itemIcon.sprite = null;
+        itemIcon.sprite = weapon.weaponSprite;
     }
 }
